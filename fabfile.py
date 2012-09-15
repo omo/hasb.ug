33685b750df3c7b2bad39d130a12565693c71f98
Hajime Morrita <morrita@gmail.com>
2012-09-14 18:32:21 -0700

    Struggling to make deployment fab commands work.

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,25 @@
 
-from fabric.api import local, run
+from fabric.api import local, run, cd
+import os
 
+# http://stackoverflow.com/questions/1180411/activate-a-virtualenv-via-fabric-as-deploy-user
+# FIXME: These should be env. variables.
+PROJECT_DIR   = "~/work/hasb.ug"
+VENV_ACTIVATE = os.path.join(PROJECT_DIR, "pyenv/bin/activate")
+
+def virtualenv(command):
+    run("source " + VENV_ACTIVATE + ' && ' + command)
+
+# Invokable commands:
 
 def hello():
     print "Hello!"
 
+def update():
+    with cd(PROJECT_DIR):
+        run("git pull")
+        run("virtualenv pyenv --distribute")
+        virtualenv("pip install -r requirements.txt")
+
+def deploy():
+    update()
