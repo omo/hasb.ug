1c50e2101598ee6760792e60f8bd6d97be9611cd
Hajime Morrita <morrita@gmail.com>
2012-10-16 23:30:04 +0900

    Fixing borken gunicorn config.

--- confs/gunicorn.conf.py
+++ confs/gunicorn.conf.py
@@ -5,6 +5,8 @@ prod_dir = "/home/ubuntu/work/hasbug/"
 worker = multiprocessing.cpu_count() * 2 + 1
 
 if os.environ.get("HASBUG_PROD"):
+    user = "ubuntu"
+    group = "ubuntu"
     bind = "0.0.0.0:80"
     accesslog  = os.path.join(prod_dir, "log/access.log")
     errorlog   = os.path.join(prod_dir, "log/error.log")
